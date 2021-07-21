9b4f14203f63a5732c643f2dced87c714c2c6424
Mahesh Dhikonda <mahesh.dhikonda@hotmail.com>
2021-07-21 22:37:40 +0530

    Add project files.

---
+++ EvenNumberFromGivennumber/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace EvenNumberFromGivennumber
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter starting number:");
+            int num = int.Parse(Console.ReadLine());
+            Console.WriteLine();
+            int n = 0;
+            while (n < 10)
+            {
+
+                num = num + 1;
+
+                if (num % 2 == 0)
+                {
+
+                    Console.Write(num + " ");
+
+                    n = n + 1;
+                }
+            }
+        }
+
+            
+
+       
+
+    
+    }
+}
